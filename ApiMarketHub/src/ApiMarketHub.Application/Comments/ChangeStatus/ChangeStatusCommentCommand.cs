fa0874ba884c55b0a16253d9128aa11a324249e3
soroush Hashemi <84335590+Soroush-hashemi@users.noreply.github.com>
2023-09-17 00:10:13 +0430

    CommentStatus CommentCommand & Handler

---
+++ ApiMarketHub/src/ApiMarketHub.Application/Comments/ChangeStatus/ChangeStatusCommentCommand.cs
@@ -0,0 +1,5 @@
+﻿using Shared.Application;
+using ApiMarketHub.Domain.CommentAggregate.Enum;
+
+namespace ApiMarketHub.Application.Comments.ChangeStatus;
+public record ChangeStatusCommentCommand(long commentId, CommentStatus changeStatus) : IBaseCommand;
