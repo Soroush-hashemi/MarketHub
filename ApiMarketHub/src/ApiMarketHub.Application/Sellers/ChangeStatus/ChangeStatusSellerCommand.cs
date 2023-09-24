6c734cf03a1776d87f504397979dfd8efbcdf6af
soroush Hashemi <84335590+Soroush-hashemi@users.noreply.github.com>
2023-09-25 00:51:39 +0330

    ChangeStatus SellerCommand & Handler

---
+++ ApiMarketHub/src/ApiMarketHub.Application/Sellers/ChangeStatus/ChangeStatusSellerCommand.cs
@@ -0,0 +1,5 @@
+﻿using ApiMarketHub.Domain.SellerAggregate.Enum;
+using Shared.Application;
+
+namespace ApiMarketHub.Application.Sellers.ChangeStatus;
+public record ChangeStatusSellerCommand(long SellerId, SellerStatus Status) : IBaseCommand;
---
+++ ApiMarketHub/src/ApiMarketHub.Application/Sellers/ChangeStatus/ChangeStatusSellerCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using ApiMarketHub.Domain.SellerAggregate.Repository;
+using Shared.Application;
+
+namespace ApiMarketHub.Application.Sellers.ChangeStatus;
+public class ChangeStatusSellerCommandHandler : IBaseCommandHandler<ChangeStatusSellerCommand>
+{
+    private readonly ISellerRepository _repository;
+    public ChangeStatusSellerCommandHandler(ISellerRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<OperationResult> Handle(ChangeStatusSellerCommand request, CancellationToken cancellationToken)
+    {
+        var seller = await _repository.GetTracking(request.SellerId);
+        if (seller == null)
+            return OperationResult.NotFound();
+
+        seller.ChangeStatus(request.Status);
+        await _repository.Save();
+        return OperationResult.Success();
+    }
+}
