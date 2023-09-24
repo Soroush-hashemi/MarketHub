7e55f86b87844426f9441538d8a63b8701205d46
soroush Hashemi <84335590+Soroush-hashemi@users.noreply.github.com>
2023-09-25 01:33:26 +0330

    AddInventory EditInventory SellerCommand & Handler

---
+++ ApiMarketHub/src/ApiMarketHub.Application/Sellers/AddInventory/AddSellerInventoryCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using ApiMarketHub.Domain.SellerAggregate;
+using ApiMarketHub.Domain.SellerAggregate.Repository;
+using Shared.Application;
+
+namespace ApiMarketHub.Application.Sellers.AddInventory;
+public class AddSellerInventoryCommandHandler : IBaseCommandHandler<AddSellerInventoryCommand>
+{
+    private readonly ISellerRepository _Repository;
+    public AddSellerInventoryCommandHandler(ISellerRepository Repository)
+    {
+        _Repository = Repository;
+    }
+
+    public async Task<OperationResult> Handle(AddSellerInventoryCommand request, CancellationToken cancellationToken)
+    {
+        var Seller = await _Repository.GetTracking(request.SellerId);
+        if (Seller == null)
+            return OperationResult.NotFound();
+
+        var inventory = new SellerInventory(request.ProductId, request.Count, request.Price, request.DiscountPercentage);
+
+        Seller.AddInventory(inventory);
+        await _Repository.Save();
+        return OperationResult.Success();
+    }
+}
---
+++ ApiMarketHub/src/ApiMarketHub.Application/Sellers/EditInventory/EditSellerInventoryCommand.cs
@@ -0,0 +1,20 @@
+﻿using Shared.Application;
+
+namespace ApiMarketHub.Application.Sellers.EditInventory;
+public class EditSellerInventoryCommand : IBaseCommand
+{
+    public long SellerId { get; private set; }
+    public long InventoryId { get; private set; }
+    public int Count { get; private set; }
+    public int Price { get; private set; }
+    public int? DiscountPercentage { get; private set; }
+
+    public EditSellerInventoryCommand(long sellerId, long inventoryId, int count, int price, int? discountPercentage)
+    {
+        SellerId = sellerId;
+        InventoryId = inventoryId;
+        Count = count;
+        Price = price;
+        DiscountPercentage = discountPercentage;
+    }
+}
