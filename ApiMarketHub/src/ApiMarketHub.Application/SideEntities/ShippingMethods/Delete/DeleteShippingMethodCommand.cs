6e8c6aecdc6d0ac4368471ce2d09527da6c745e9
soroush Hashemi <84335590+Soroush-hashemi@users.noreply.github.com>
2023-09-24 22:29:55 +0330

    Delete ShippingMethodCommand & Handler

---
+++ ApiMarketHub/src/ApiMarketHub.Application/SideEntities/ShippingMethods/Delete/DeleteShippingMethodCommand.cs
@@ -0,0 +1,9 @@
+﻿using ApiMarketHub.Application.SideEntities.ShippingMethods.Edit;
+using ApiMarketHub.Domain.SideEntities.Repository;
+using Shared.Application;
+
+namespace ApiMarketHub.Application.SideEntities.ShippingMethods.Delete;
+public class DeleteShippingMethodCommand : IBaseCommand
+{
+    public long Id { get; set; }
+}
---
+++ ApiMarketHub/src/ApiMarketHub.Application/SideEntities/ShippingMethods/Delete/DeleteShippingMethodCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using ApiMarketHub.Domain.SideEntities.Repository;
+using Shared.Application;
+
+namespace ApiMarketHub.Application.SideEntities.ShippingMethods.Delete;
+public class DeleteShippingMethodCommandHandler : IBaseCommandHandler<DeleteShippingMethodCommand>
+{
+    private readonly IShippingMethodRepository _repository;
+    public DeleteShippingMethodCommandHandler(IShippingMethodRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<OperationResult> Handle(DeleteShippingMethodCommand request, CancellationToken cancellationToken)
+    {
+        var shippingMethod = await _repository.GetTracking(request.Id);
+        if (shippingMethod == null)
+            return OperationResult.NotFound();
+
+        _repository.Delete(shippingMethod);
+        await _repository.Save();
+        return OperationResult.Success();
+    }
+}
