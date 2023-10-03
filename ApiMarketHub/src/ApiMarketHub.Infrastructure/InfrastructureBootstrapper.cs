60e0dd0317e570dfdaef45a1506a04eac8d8cdad
soroush Hashemi <84335590+Soroush-hashemi@users.noreply.github.com>
2023-10-03 14:29:28 +0330

    InfrastructureBootstrapper

---
+++ ApiMarketHub/src/ApiMarketHub.Infrastructure/InfrastructureBootstrapper.cs
@@ -0,0 +1,35 @@
+﻿using ApiMarketHub.Domain.CategoryAggregate.Repository;
+using ApiMarketHub.Domain.CommentAggregate.Repository;
+using ApiMarketHub.Domain.OrderAggregate.Repository;
+using ApiMarketHub.Domain.ProductAggregate.Repository;
+using ApiMarketHub.Domain.RoleAggregate.Repository;
+using ApiMarketHub.Domain.SellerAggregate.Repository;
+using ApiMarketHub.Domain.SideEntities.Repository;
+using ApiMarketHub.Domain.UserAggregate.Repository;
+using ApiMarketHub.Infrastructure.Persistence.Command;
+using ApiMarketHub.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ApiMarketHub.Infrastructure;
+public class InfrastructureBootstrapper
+{
+    public static void Init(IServiceCollection services, string connectionString)
+    {
+        services.AddTransient<ICategoryRepository, CategoryRepository>();
+        services.AddTransient<IOrderRepository, OrderRepository>();
+        services.AddTransient<IProductRepository, ProductRepository>();
+        services.AddTransient<IRoleRepository, RoleRepository>();
+        services.AddTransient<ISellerRepository, SellerRepository>();
+        services.AddTransient<IPosterRepository, PosterRepository>();
+        services.AddTransient<ISliderRepository, SliderRepository>();
+        services.AddTransient<IUserRepository, UserRepository>();
+        services.AddTransient<ICommentRepository, CommentRepository>();
+        services.AddTransient<IShippingMethodRepository, ShippingMethodRepository>();
+
+        services.AddDbContext<Context>(option =>
+        {
+            option.UseSqlServer(connectionString);
+        });
+    }
+}
