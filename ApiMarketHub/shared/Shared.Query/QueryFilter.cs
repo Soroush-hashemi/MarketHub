76fadd4b2810bff0a839ef7c977c3bfc02cd1e2e
soroush Hashemi <84335590+Soroush-hashemi@users.noreply.github.com>
2023-10-06 19:30:43 +0330

    Create FilterBase in Shared.Query

---
+++ ApiMarketHub/shared/Shared.Query/QueryFilter.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Shared.Query.Bases;
+
+namespace Shared.Query;
+public class QueryFilter<TResponse, TParam> : IQuery<TResponse>
+    where TResponse : BaseFilter
+    where TParam : BaseFilterParam
+{
+    public TParam FilterParams { get; set; }
+    public QueryFilter(TParam filterParams)
+    {
+        FilterParams = filterParams;
+    }
+}
