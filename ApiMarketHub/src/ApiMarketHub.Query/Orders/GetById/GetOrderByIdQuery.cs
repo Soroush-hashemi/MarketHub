a4a41084a99e4415e5f64bb67c10f77b62fc3c13
soroush Hashemi <84335590+Soroush-hashemi@users.noreply.github.com>
2023-10-09 00:24:16 +0330

    Add GetOrderByIdQuery & Handler to Order Query

---
+++ ApiMarketHub/src/ApiMarketHub.Query/Orders/GetById/GetOrderByIdQuery.cs
@@ -0,0 +1,5 @@
+﻿using ApiMarketHub.Query.Orders.DTOs;
+using Shared.Query;
+
+namespace ApiMarketHub.Query.Orders.GetById;
+public record GetOrderByIdQuery(long orderId) : IQuery<OrderDto?>;
---
+++ ApiMarketHub/src/ApiMarketHub.Query/Orders/GetById/GetOrderByIdQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using ApiMarketHub.Infrastructure.Persistence.Command;
+using ApiMarketHub.Infrastructure.Persistence.Query;
+using ApiMarketHub.Query.Orders.DTOs;
+using Microsoft.EntityFrameworkCore;
+using Shared.Query;
+
+namespace ApiMarketHub.Query.Orders.GetById;
+public class GetOrderByIdQueryHandler : IQueryHandler<GetOrderByIdQuery, OrderDto?>
+{
+    private readonly Context _context;
+    private readonly DapperContext _dapperContext;
+
+    public GetOrderByIdQueryHandler(Context context, DapperContext dapperContext)
+    {
+        _context = context;
+        _dapperContext = dapperContext;
+    }
+
+    public async Task<OrderDto?> Handle(GetOrderByIdQuery request, CancellationToken cancellationToken)
+    {
+        var order = await _context.Orders.FirstOrDefaultAsync(o => o.Id == request.orderId);
+        if (order == null)
+            throw new NullReferenceException();
+
+        var orderDto = order.Map();
+
+        orderDto.UserFullName = await _context.Users.Where(u => u.Id == orderDto.UserId)
+            .Select(u => $"{u.Name} {u.Family}").FirstAsync(cancellationToken);
+
+        orderDto.Items = await orderDto.GetOrderItems(_dapperContext);
+
+        return orderDto;
+    }
+}
