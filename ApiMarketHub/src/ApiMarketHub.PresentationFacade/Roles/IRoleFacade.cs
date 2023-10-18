0c9a4e82eaa634755b726a050c46a041dba95985
soroush Hashemi <84335590+Soroush-hashemi@users.noreply.github.com>
2023-10-18 17:23:20 +0330

    Make RoleFacade

---
+++ ApiMarketHub/src/ApiMarketHub.PresentationFacade/Roles/IRoleFacade.cs
@@ -0,0 +1,14 @@
+﻿using ApiMarketHub.Application.Roles.Create;
+using ApiMarketHub.Application.Roles.Edit;
+using ApiMarketHub.Query.Roles.DTO;
+using Shared.Application;
+
+namespace ApiMarketHub.PresentationFacade.Roles;
+public interface IRoleFacade
+{
+    Task<OperationResult> Create(CreateRoleCommand command);
+    Task<OperationResult> Edit(EditRoleCommand command);
+
+    Task<RoleDto?> GetRoleById(long RoleId);
+    Task<List<RoleDto>> GetRoles();
+}
---
+++ ApiMarketHub/src/ApiMarketHub.PresentationFacade/Roles/RoleFacade.cs
@@ -0,0 +1,37 @@
+﻿using ApiMarketHub.Application.Roles.Create;
+using ApiMarketHub.Application.Roles.Edit;
+using ApiMarketHub.Query.Roles.DTO;
+using ApiMarketHub.Query.Roles.GetById;
+using ApiMarketHub.Query.Roles.GetList;
+using MediatR;
+using Shared.Application;
+
+namespace ApiMarketHub.PresentationFacade.Roles;
+internal class RoleFacade : IRoleFacade
+{
+    private readonly Mediator _mediator;
+    public RoleFacade(Mediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    public async Task<OperationResult> Create(CreateRoleCommand command)
+    {
+        return await _mediator.Send(command);
+    }
+
+    public async Task<OperationResult> Edit(EditRoleCommand command)
+    {
+        return await _mediator.Send(command);
+    }
+
+    public async Task<RoleDto?> GetRoleById(long RoleId)
+    {
+        return await _mediator.Send(new GetRoleByIdQuery(RoleId));
+    }
+
+    public async Task<List<RoleDto>> GetRoles()
+    {
+        return await _mediator.Send(new GetRoleListQuery());
+    }
+}
